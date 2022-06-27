a92324c6604c71d1f5c5a1dfb2383eeffb8ade3c
홍근호 <khhong@deleo.co.kr>
2022-06-27 16:04:40 +0900

    feat: Box 컴포넌트 추가
    다형성 Box 컴포넌트 추가
    props 추가는 추후 진행
    
    ref:#4-box

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -7,11 +7,11 @@ module.exports = {
   useTabs: false,
   semi: true,
   singleQuote: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   bracketSpacing: true,
   bracketSameLine: true,
-  plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
-  importOrder: ["^@plasmohq/(.*)$", "^~(.*)$", "^[./]"],
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: ['^@plasmohq/(.*)$', '^~(.*)$', '^[./]'],
   importOrderSeparation: true,
-  importOrderSortSpecifiers: true
-}
+  importOrderSortSpecifiers: true,
+};
