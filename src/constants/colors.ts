1071e2ae013b78ebbf68e4c89ea5330d7b550219
jtpark <jtpark@deleo.co.kr>
2022-06-28 16:41:28 +0900

    feat: 버튼 color 적용
    버튼 color props 설정 추가
    
    ref #5-button

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const enum THEME_COLOR {
+  DEFAULT = 'default',
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  ERROR = 'error',
+  WARNING = 'warning',
+  INFO = 'info',
+  SUCCESS = 'success',
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,7 @@
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 
+import Button from '~components/atoms/button';
+import { THEME_COLOR } from '~constants/colors';
 import { useCounterStore } from '~store';
 
 export const Home = () => {
@@ -16,16 +18,19 @@ export const Home = () => {
   return (
     <div style={{ padding: 16 }}>
       <span>Home page111</span>
-      <button onClick={onNextPage}>About</button>
-      <button
+      <Button onClick={onNextPage}>About</Button>
+      <Button
+        color={THEME_COLOR.ERROR}
         className="font-bold pl-5"
         onClick={() => {
           navigation('/hong');
         }}>
         Hong
-      </button>
+      </Button>
       <div>{bears}</div>
-      <button onClick={increasePopulation}>Click!</button>
+      <Button color={THEME_COLOR.WARNING} onClick={increasePopulation}>
+        Click!
+      </Button>
     </div>
   );
 };
