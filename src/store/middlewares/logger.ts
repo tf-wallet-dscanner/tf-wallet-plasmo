831b4c0ea581b70d124f4a3f7917ab6225f4b0d8
jtpark <jtpark@deleo.co.kr>
2022-06-28 10:16:00 +0900

    feat: logger name 추가
    상태가 변경된 logger를 구분하기 위한 logger name parameter 추가

--- src/store/middlewares/logger.ts
+++ src/store/middlewares/logger.ts
@@ -10,20 +10,37 @@ const enum CONSOLE_COLOR {
   GRAY = '#999',
   BLUE = '#2e9fff',
   GREEN = '#41b838',
+  RED = '#f75757',
 }
 
 const consoleStyle = (color: CONSOLE_COLOR) =>
   `color:${color}; font-weight: bold`;
 
 const logger =
-  <S extends State>(config: StateCreator<S>) =>
+  <S extends State>(config: StateCreator<S>, name?: string) =>
   (set: SetState<S>, get: GetState<S>, api: StoreApi<S>) =>
     config(
       (args) => {
-        console.log('%c  prev state', consoleStyle(CONSOLE_COLOR.GRAY), get());
-        console.log('%c  action    ', consoleStyle(CONSOLE_COLOR.BLUE), args);
+        const prefix = name ? `[${name}]` : '  ';
+        console.log(
+          `%c${prefix}%c  prev state`,
+          consoleStyle(CONSOLE_COLOR.RED),
+          consoleStyle(CONSOLE_COLOR.GRAY),
+          get(),
+        );
+        console.log(
+          `%c${prefix}%c  action    `,
+          consoleStyle(CONSOLE_COLOR.RED),
+          consoleStyle(CONSOLE_COLOR.BLUE),
+          args,
+        );
         set(args);
-        console.log('%c  next state', consoleStyle(CONSOLE_COLOR.GREEN), get());
+        console.log(
+          `%c${prefix}%c  next state`,
+          consoleStyle(CONSOLE_COLOR.RED),
+          consoleStyle(CONSOLE_COLOR.GREEN),
+          get(),
+        );
       },
       get,
       api,
