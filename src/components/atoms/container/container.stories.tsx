fd118162cdbe06c17bd7a8da62e8024d1246699f
jtpark <jtpark@deleo.co.kr>
2022-06-29 16:07:25 +0900

    feat: container component 추가
    popup layout을 잡는데 사용하는 container 컴포넌트 추가
    
    ref #9-container

---
+++ src/components/atoms/container/container.stories.tsx
@@ -0,0 +1,23 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import Container from './container';
+
+export default {
+  title: 'Atoms/Container',
+  component: Container,
+} as ComponentMeta<typeof Container>;
+
+const Template: ComponentStory<typeof Container> = (args) => (
+  <Container {...args} />
+);
+
+export const Basic = Template.bind({});
+Basic.args = {
+  children: 'Basic Container',
+};
+
+export const Polymorphism = Template.bind({});
+Polymorphism.args = {
+  children: 'Polymorphism Container',
+  as: 'section',
+};
